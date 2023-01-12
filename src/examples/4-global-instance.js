2a90e4a4e9345f3f40ec7104a60b0ab3df3cb5d5
Felix-fale <falefelix23@gmail.com>
2023-01-12 00:50:29 +0000

    feat: Custom Instance

--- src/examples/4-global-instance.js
+++ src/examples/4-global-instance.js
@@ -10,7 +10,7 @@ const GlobalInstance = () => {
     try {
       const resp1 = await axios(productsUrl);
       const resp2 = await axios(randomUserUrl);
-      console.log(resp1);
+      console.log(resp1, resp2);
     } catch (error) {}
   };
 
